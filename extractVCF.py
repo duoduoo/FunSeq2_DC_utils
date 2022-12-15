a7b7fab961ad4952e563bfc5c9086851639254ad
Duo 'Erica' Xu <xdd.erica@gmail.com>
2022-12-15 12:21:45 -0500

    Add files via upload
    Extract information from the output of FunSeq2 (vcf format, "sampleID.vcf") for CNCDriver for one sample.
    
    Usage: python extractVCF.py sampleID

---
+++ extractVCF.py
@@ -0,0 +1,108 @@
+
+import pandas as pd
+import re, sys
+import numpy as np
+from collections import Counter
+import random
+import scipy.stats as stats
+from scipy.stats import norm
+from scipy import interp
+import pickle
+import time
+import os
+
+tempPath = os.environ['TMPDIR']
+
+vcfCol = ['chr','posStart','posEnd','ref','alt','sampleID','CDS','VA','HUB','GNEG','GERP','UCONS','HOT','SEN','USEN','GENE','CDSS','NCDS','NCENC','MOTIFBR','MOTIFG','RECUR','DBRECUR','tumorType']
+len(vcfCol)
+
+eachPatient = str(sys.argv[1])
+inputVCF=tempPath+'/'+eachPatient+'.vcf'
+outputCDS=tempPath+'/'+eachPatient+'_CDS.txt'
+outputNCDS=tempPath+'/'+eachPatient+'_NCDS.txt'
+
+
+with open(inputVCF, 'r') as fin, open(outputCDS, 'w') as foutCDS, open(outputNCDS,'w') as foutNCDS:
+    foutCDS.write('\t'.join(vcfCol)+'\n')
+    foutNCDS.write('\t'.join(vcfCol)+'\n')
+    for line in fin:
+        if line.startswith('#'):
+            continue
+        
+        #         print(line)
+        line=line.strip()
+        fields = line.split('\t')
+        
+        chrom=fields[0]
+        posStart=fields[1]
+        posEnd=fields[1]
+        ref=fields[3]
+        alt=fields[4]
+        
+        
+        infoList = fields[7].split(';')
+        infoOut = ['NA']*19
+        
+        for info in infoList:
+            if info.startswith('SAMPLE'):
+                infoOut[0]=info.split('=')[1].replace('--*','')
+            elif info.startswith('CDS='):
+                infoOut[1]=info.split('=')[1]
+                cdsStatus=info.split('=')[1]
+            
+            elif info.startswith('VA'):
+                infoOut[2]=info.split('=')[1]
+            elif info.startswith('HUB'):
+                infoOut[3]=info.split('=')[1]
+            elif info.startswith('GNEG'):
+                infoOut[4]=info.split('=')[1]
+            elif info.startswith('GERP'):
+                infoOut[5]=info.split('=')[1]
+            elif info.startswith('UCONS'):
+                infoOut[6]=info.split('=')[1]
+            elif info.startswith('HOT'):
+                infoOut[7]=info.split('=')[1]
+            elif info.startswith('SEN'):
+                infoOut[8]=info.split('=')[1]
+            elif info.startswith('USEN'):
+                infoOut[9]=info.split('=')[1]
+            elif info.startswith('GENE'):
+                infoOut[10]=re.sub('\[[^\[]+\]','', info.split('=')[1])
+            elif info.startswith('CDSS'):
+                infoOut[11]=info.split('=')[1]
+            elif info.startswith('NCDS'):
+                infoOut[12]=info.split('=')[1]
+            elif info.startswith('NCENC'):
+                infoOut[13]=re.sub('\[[^\[]+\]','', info.split('=')[1])
+            elif info.startswith('MOTIFBR'):
+                infoOut[14]=info.split('=')[1]
+            elif info.startswith('MOTIFG'):
+                infoOut[15]=info.split('=')[1]
+            elif info.startswith('RECUR'):
+                infoOut[16]=info.split('=')[1]
+            elif info.startswith('DBRECUR'):
+                infoOut[17]=info.split('=')[1]
+            else:
+                continue
+
+        infoOut[18] = 'XXXX\n'
+        
+        if infoOut[16] != 'NA':
+            infoOut[16]='Yes'
+        else:
+            infoOut[16]='No'
+        if infoOut[17] != 'NA':
+            infoOut[17]='Yes'
+        else:
+            infoOut[17]='No'
+        
+
+        if cdsStatus == 'Yes':
+            foutCDS.write('\t'.join([chrom,posStart,posEnd,ref,alt])+'\t')
+            foutCDS.write('\t'.join(infoOut))
+        elif cdsStatus == 'No':
+            foutNCDS.write('\t'.join([chrom,posStart,posEnd,ref,alt])+'\t')
+            foutNCDS.write('\t'.join(infoOut))
+        else:
+            print('Error: something is wrong')
+
